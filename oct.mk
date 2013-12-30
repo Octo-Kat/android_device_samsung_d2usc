b8583b601dfe19b40b0b543b2890e34412d444bd
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-30 13:04:21 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -8,6 +8,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys" PRIVATE_BUILD_DESC="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys"
 
-PRODUCT_NAME := cm_d2usc
+PRODUCT_NAME := oct_d2usc
 PRODUCT_DEVICE := d2usc
 
